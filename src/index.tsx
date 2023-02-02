60a4d59effa6bfa114ebe5042e53446d45c29c25
Batyr Kanzitdinov <dev@batyr.io>
2023-02-02 02:34:50 +0100

    rc.5

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,6 @@ import {NavioScreen} from './types';
 export {Navio};
 export type {NavioScreen};
 
-// TODO There is a problem when there are more than 2 drawers or 2 tabs, then .jumpTo() won't help with autocompletion
+// todos
+// TODO There is a problem when there are more than 2 drawers or 2 tabs, then .jumpTo() won't help with autocompletion.
+// TODO BottomTabNavigatorProps and DrawerNavigatorProps are missing from react-navigation exports. Type `any` is currently used.
