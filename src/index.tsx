2f9447dfc83e72edfac2409a6b33197abfdd4007
Batyr Kanzitdinov <dev@batyr.io>
2023-02-03 20:17:23 +0100

    readme

--- src/index.tsx
+++ src/index.tsx
@@ -4,46 +4,8 @@ import {NavioScreen} from './types';
 export {Navio};
 export type {NavioScreen};
 
-// 0.0.2 todos
-// [ ] prepare docs
-
 // minor issues
 // TODO [ISSUE][TS] When there are more than 2 drawers or 2 tabs, then `.jumpTo()` won't help with autocompletion.
 // TODO [ISSUE][TS] `BottomTabNavigatorProps` and `DrawerNavigatorProps` are missing from react-navigation exports. Type `any` is currently used.
 // TODO [ISSUE][TS] `RootName` is `string` when one of [stacks, tabs, drawers] is not defined. Fix: just initialise them as empty object {stacks: {}, tabs: {}, drawers: {}}
 // TODO [ISSUE][TS] `navio.setParams` - think about how to take all names and not getting plain string when some name is not defined. It's related to `RootName` issue.
-
-// testing
-// const navio = Navio.build({
-//   screens: {
-//     Main: {
-//       component: () => {
-//         return null;
-//       },
-//     },
-//   },
-//   stacks: {
-//     MainStack: ['Main'],
-//   },
-//   tabs: {
-//     MainTabs: {
-//       content: {
-//         One: 'MainStack',
-//         Two: 'MainStack',
-//       },
-//     },
-//   },
-//   drawers: {
-//     MainDrawer: {
-//       content: {
-//         One: 'MainStack',
-//         Two: ['Main'],
-//       },
-//     },
-//   },
-//   root: 'MainDrawer',
-// });
-// navio.tabs.updateOptions('One', {'tabBarBadge': '123'})
-// navio.setRoot('stacks', 'MainStack');
-// navio.Root({'initialRouteName':''})
-// navio.drawers.setRoot('')
